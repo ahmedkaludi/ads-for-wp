7d6370578c6062984d838a178b35c5909ebe3903
Mohammed Kaludi <mohammed@magazine3.com>
2017-10-26 18:49:27 +0530

    Initial File added

---
+++ ads-for-wp.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Ads for WP
+Plugin URI: https://wordpress.org/plugins/ads-for-wp/
+Description: Ads for WP - The best Advertisement plugin in WordPress
+Version: 0.1
+Author: Ahmed Kaludi, Mohammed Kaludi
+Author URI: https://ampforwp.com/
+Donate link: https://www.paypal.me/Kaludi/25
+License: GPL2+
+*/
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
