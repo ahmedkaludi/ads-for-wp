d93109a02cb56f1ff97f0aeacf4accfb66ffebff
Mohammed Kaludi <mohammed@magazine3.com>
2017-10-26 19:22:11 +0530

    Commeting and widget file added
    Commeting and widget file added

--- ads-for-wp.php
+++ ads-for-wp.php
@@ -12,3 +12,13 @@ License: GPL2+
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) exit;
+
+define('ADSFORWP_PLUGIN_DIR', plugin_dir_path( __FILE__ ));
+define('ADSFORWP_PLUGIN_DIR_URI', plugin_dir_url(__FILE__));
+define('ADSFORWP_IMAGE_DIR',plugin_dir_url(__FILE__).'images');
+define('ADSFORWP_PLUGIN_DIR_PATH', plugin_dir_path( __DIR__ ) );
+define('ADSFORWP_VERSION','0.1');
+
+
+/* Adding Files*/
+require ( ADSFORWP_PLUGIN_DIR.'/widget/ads-widget.php' );
