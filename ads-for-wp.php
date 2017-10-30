c5f087ae5956eb495ad078e4fbee025f707d63d6
Mohammed Kaludi <mohammed@magazine3.com>
2017-10-30 19:16:30 +0530

    Metabox created for Ads on/off on all post types
    Metabox created for Ads on/off on all post types

--- ads-for-wp.php
+++ ads-for-wp.php
@@ -25,6 +25,7 @@ require ( ADSFORWP_PLUGIN_DIR.'/widget/ads-widget.php' );
 // Options panel
 // require ( ADSFORWP_PLUGIN_DIR.'/admin/settings/ads-settings.php' );
 require ( ADSFORWP_PLUGIN_DIR.'/admin/control-center.php' );
+require ( ADSFORWP_PLUGIN_DIR.'/admin/global-metaboxes.php' );
 
 
 // add_filter('adsforwp_advert_on_off', 'modify_code');
@@ -33,4 +34,35 @@ function modify_code($show) {
 		$show = 'no';
 	}
 	return $show;
+}
+
+
+// Get all the post types and add metaboxs of the ads in this post types
+add_action('admin_init', 'adsforwp_generate_postype');
+function adsforwp_generate_postype(){
+	adsforwp_post_types();
+}
+
+function adsforwp_post_types(){
+	$args 			= "";
+	$get_post_types = "";
+	$post_types 	= "";
+
+	$args = array(
+	   'public'   => true,
+	);
+
+	$get_post_types = get_post_types( $args, 'objects');
+
+	// Remove the unwanted post types 
+	unset($get_post_types['attachment']);
+	unset($get_post_types['ads-for-wp-ads']);
+
+	foreach ( $get_post_types  as $post_type ) {
+		$post_types[$post_type->name] = $post_type->label;
+	}
+
+	$post_types = apply_filters( 'adsforwp_modify_post_types', $post_types );
+
+	return $post_types;
 }
