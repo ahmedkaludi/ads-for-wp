ddcc7d932cae82f3486b154f55a156bc2aa6e901
Sejiro-Hiko <mohdasif7026@gmail.com>
2018-03-01 14:45:15 +0530

    Version number changed

--- ads-for-wp.php
+++ ads-for-wp.php
@@ -3,7 +3,7 @@
 Plugin Name: Ads for WP
 Plugin URI: https://wordpress.org/plugins/ads-for-wp/
 Description: Ads for WP - The best Advertisement plugin in WordPress
-Version: 0.1
+Version: 2.0 beta
 Author: Ahmed Kaludi, Mohammed Kaludi
 Author URI: https://ampforwp.com/
 Donate link: https://www.paypal.me/Kaludi/25
