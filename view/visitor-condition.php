281f25ccab11691cec99cc9ef7ef93dca789138d
santosh8793 <santosh.veggalam@gmail.com>
2019-10-16 13:13:13 +0530

    #261 Added code for mobile version detection

--- view/visitor-condition.php
+++ view/visitor-condition.php
@@ -261,27 +261,21 @@ class adsforwp_view_visitor_condition {
         switch ($type) {
                    
           case 'device':
-              if(function_exists('ampforwp_is_amp_endpoint')){
-                require_once AMPFORWP_PLUGIN_DIR.'/includes/vendor/Mobile_Detect.php';
-                $mobile_detect = $isTablet = '';
-                // instantiate the Mobile detect class
-                $mobile_detect = new AMPforWP_Mobile_Detect;
-                $isMobile = $mobile_detect->isMobile();
-                $isTablet = $mobile_detect->isTablet();
+              
+              require_once ADSFORWP_PLUGIN_DIR.'/vendor/mobile-detect.php';
+              $mobile_detect = $isTablet = '';
+              // instantiate the Mobile detect class
+              $mobile_detect = new Adsforwp_Mobile_Detect;
+              $isMobile = $mobile_detect->isMobile();
+              $isTablet = $mobile_detect->isTablet();
 
-                $device_name  = 'desktop';
-                if( $isMobile && $isTablet ){ //Only For tablet
-                  $device_name  = 'mobile';
-                }else if($isMobile && !$isTablet){ // Only for mobile
-                  $device_name  = 'mobile';
-                }
-              }else{
-                $device_name  = 'desktop'; 
-                if(wp_is_mobile()){
-                $device_name  = 'mobile';                
-                } 
+              $device_name  = 'desktop';
+              if( $isMobile && $isTablet ){ //Only For tablet
+                $device_name  = 'mobile';
+              }else if($isMobile && !$isTablet){ // Only for mobile
+                $device_name  = 'mobile';
               }
-                                
+                           
               if ( $comparison == 'equal' ) {
                   if ( $device_name == $data ) {
                     $result = true;
