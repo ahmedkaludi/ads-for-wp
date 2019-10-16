251b1dec48792471bce7569ff9650c9a4d87796d
santosh8793 <santosh.veggalam@gmail.com>
2019-10-16 15:48:13 +0530

    #272 Added code for heading tags count

--- output/functions.php
+++ output/functions.php
@@ -28,6 +28,12 @@ class adsforwp_output_functions{
                     '</p>'    => 'p_tag',
                     '</div>'  => 'div_tag',
                     '<img>'   => 'img_tag',                    
+                    '</h1>'   => 'h1',                    
+                    '</h2>'   => 'h2',                    
+                    '</h3>'   => 'h3',                    
+                    '</h4>'   => 'h4',                    
+                    '</h5>'   => 'h5',                    
+                    '</h6>'   => 'h6',                    
                     );
                           
     }
--- view/display.php
+++ view/display.php
@@ -54,8 +54,14 @@ class adsforwp_view_display {
 			'options' => array(                               
 				'p_tag'=>'p (default)',
 				'div_tag'=>'div', 
-                                'img_tag'=>'img',
-                                'custom_tag'=>'custom',
+                'img_tag'=>'img',
+                'h1' => 'H1',
+                'h2' => 'H2',
+                'h3' => 'H3',
+                'h4' => 'H4',
+                'h5' => 'H5',
+                'h6' => 'H6',
+                'custom_tag'=>'custom',
 			),
 		),
                 array(
